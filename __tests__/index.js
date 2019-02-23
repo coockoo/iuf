48d7578d70648f7f704f085b2c99abd0d4572970
Alexandr Bondarchuk <alex.coockoo@gmail.com>
2019-02-23 22:47:52 +0200

    Add tests with jest :check:

---
+++ __tests__/index.js
@@ -0,0 +1,52 @@
+const iuf = require('../src');
+
+test('does valid mergeIn', () => {
+  const obj = { a: { b: { c: 15 }, b1: 20 }, a1: 'x' };
+  const newObj = iuf.mergeIn(obj, ['a', 'b'], { c: 10, d: 12 });
+  expect(newObj.a.b.c).toBe(10);
+  expect(newObj.a.b.d).toBe(12);
+  expect(newObj).not.toBe(obj);
+});
+
+test('does valid pushIn', () => {
+  const obj = { a: { b: { c: [1, 2, 3] }, b1: 20 }, a1: 'x' };
+  const newObj = iuf.pushIn(obj, ['a', 'b', 'c'], 4);
+  expect(newObj.a.b.c).toHaveLength(4);
+  expect(newObj.a.b.c[3]).toEqual(4);
+  expect(newObj.a.b.c).toBeInstanceOf(Array);
+  expect(newObj).not.toBe(obj);
+});
+
+test('does valid setIn', () => {
+  const obj = { a: { b: { c: 15 }, b1: 20 }, a1: 'x' };
+  const newObj = iuf.setIn(obj, ['a', 'b', 'c'], 16);
+  expect(newObj.a.b.c).toEqual(16);
+  expect(newObj).not.toBe(obj);
+});
+
+test('does valid sliceIn', () => {
+  const obj = { a: { b: { c: [1, 2, 3] }, b1: 20 }, a1: 'x' };
+  const newObj = iuf.sliceIn(obj, ['a', 'b', 'c'], 1);
+  expect(newObj.a.b.c).toHaveLength(2);
+  expect(newObj.a.b.c[0]).toEqual(1);
+  expect(newObj.a.b.c[1]).toEqual(3);
+  expect(newObj.a.b.c).toBeInstanceOf(Array);
+  expect(newObj).not.toBe(obj);
+});
+
+test('does valid toggleIn', () => {
+  const obj = { a: { b: { c: false }, b1: 20 }, a1: 'x' };
+  const newObj = iuf.toggleIn(obj, ['a', 'b', 'c']);
+  expect(newObj.a.b.c).toEqual(true);
+  expect(newObj).not.toBe(obj);
+});
+
+test('does valid updateInWith', () => {
+  function doUpdate (value) {
+    return value > 0 ? value - 5 : value + 10;
+  }
+  const obj = { a: { b: { c: 10 }, b1: 20 }, a1: 'x' };
+  const newObj = iuf.updateInWith(obj, ['a', 'b', 'c'], doUpdate);
+  expect(newObj.a.b.c).toEqual(5);
+  expect(newObj).not.toBe(obj);
+});
